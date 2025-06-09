8f8a125669a07f565fb1644ae9ef4bcf4274497b
Prathmesh1524 <166718793+Prathmesh1524@users.noreply.github.com>
2025-06-09 11:14:04 +0530

    added resume

--- component/CvButton.jsx
+++ component/CvButton.jsx
@@ -4,32 +4,39 @@ import { FiDownload } from "react-icons/fi";
 
 export default function CvButton() {
   return (
-    <button
-      className="
-        px-3 py-2
-        text-[12px] 
-        uppercase 
-        tracking-[2.5px] 
-        font-lg
-        font-bold
-        pointer-cursor
-        text-black 
-        bg-white 
-        rounded-full 
-        shadow-[0px_8px_15px_rgba(0,0,0,0.1)] 
-        transition 
-        duration-300 
-        ease-in-out 
-        hover:bg-[#23c483] 
-        hover:shadow-[0px_15px_20px_rgba(46,229,157,0.4)] 
-        hover:text-white 
-        hover:-translate-y-[7px] 
-        active:translate-y-[-1px] 
-        focus:outline-none
-      "
+    <a
+      href="https://drive.google.com/drive/u/0/folders/1ydYbJH22DHoyp1iR-HXOm6LL-pmCa3so"
+      target="_blank"
+      rel="noopener noreferrer"
     >
-   <span className="flex gap-2 ">  Download CV <FiDownload className="mt-0.5 text-lg"/></span>
-     
-    </button>
+      <button
+        className="
+          px-3 py-2
+          text-[12px] 
+          uppercase 
+          tracking-[2.5px] 
+          font-lg
+          font-bold
+          pointer-cursor
+          text-black 
+          bg-white 
+          rounded-full 
+          shadow-[0px_8px_15px_rgba(0,0,0,0.1)] 
+          transition 
+          duration-300 
+          ease-in-out 
+          hover:bg-[#23c483] 
+          hover:shadow-[0px_15px_20px_rgba(46,229,157,0.4)] 
+          hover:text-white 
+          hover:-translate-y-[7px] 
+          active:translate-y-[-1px] 
+          focus:outline-none
+        "
+      >
+        <span className="flex gap-2 items-center">
+          Download CV <FiDownload className="text-lg" />
+        </span>
+      </button>
+    </a>
   );
 }
